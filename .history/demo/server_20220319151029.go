15c37b44d636c34d8bf64c3e8f37ea704c244f85
ReigenDing <1066297896@qq.com>
2022-03-25 07:59:55 +0800

    Remove useless file

--- .history/demo/server_20220319151029.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func main() {
-	server := znet.NewServer("zinx-v0.0.1", 9000)
-
-}
--- .history/demo/server_20220319152712.go
+++
@@ -1,8 +0,0 @@
-package main
-
-func main() {
-	server := znet.NewServer("zinx-v0.0.1", 9000)
-
-	server.Server()
-
-}
